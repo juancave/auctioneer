4787083841505874eb65e19c4c847d78e33f33ab
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-01-26 15:45:18 -0500

    Adding modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { UserModule } from './user/user.module';
+import { BidModule } from './bid/bid.module';
+import { AuctionModule } from './auction/auction.module';
+
 @Module({
-  imports: [],
+  imports: [UserModule, BidModule, AuctionModule],
   controllers: [AppController],
   providers: [AppService],
 })
