fb1cf9807a1591102f30be502c6eb642fce0db7c
Juan Camilo Velásquez <juan.velasquez@gorillalogic.com>
2024-02-06 17:26:38 -0500

    Adding ORM module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,23 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserModule } from './user/user.module';
 import { BidModule } from './bid/bid.module';
 import { AuctionModule } from './auction/auction.module';
 import { LoggerMiddleware } from './shared/middlewares/logger.middleware';
+import { TypeOrmConfigService } from './database/typeorm-config.service';
 
 @Module({
-  imports: [UserModule, BidModule, AuctionModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    UserModule,
+    BidModule,
+    AuctionModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
